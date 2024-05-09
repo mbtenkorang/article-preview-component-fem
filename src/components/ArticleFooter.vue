75f4534273dcbfa928e12f63fa2149639b352b71
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-05-09 20:40:02 +0000

    Footer for mobile with vue animation

--- src/components/ArticleFooter.vue
+++ src/components/ArticleFooter.vue
@@ -1,4 +1,11 @@
 <script setup>
+import {ref, Transition} from 'vue';
+import facebookLogo from "../assets/images/icon-facebook.svg";
+import  twitterLogo from "../assets/images/icon-twitter.svg";
+import pinterestLogo from "../assets/images/icon-pinterest.svg";
+import shareIcon from "../assets/images/icon-share.svg";
+
+
 const props = defineProps({
     authorname: {
         type: String,
@@ -11,16 +18,19 @@ const props = defineProps({
     avatarurl: {
         type: String,
         required: true
-    },
-    shareicon: {
-        type: String,
-        required: true
     }
 })
+
+const show = ref(true);
+
+const handleShowElement = () =>{
+    show.value = !show.value;
+}
 </script>
 
 <template>
-    <div class="article-footer">
+    <Transition name="slide">
+    <div  v-if="show" class="article-footer">
         <div class="author-info">
           <img :src="avatarurl" :alt="`${authorname}`" class="author-info img">
             <div>
@@ -28,10 +38,36 @@ const props = defineProps({
                 <span>{{ articledate }}</span>
             </div>  
         </div>
-        <div class="share-icon">
-            <img :src="shareicon" alt="">
+        <div class="bg-lightgrayblue share-icon" @click="handleShowElement">
+            <img :src="shareIcon" alt="">
         </div>
     </div>
+    <div v-else class="article-footer bg-darkgrayblue mx-0 px-7 pt-6">
+        <p  class="uppercase tracking-[5px] text-lightgrayblue text-2xl">share</p>
+        <div class="flex items-baseline">
+        <a href="https://facebook.com" target="_blank" class="mr-6"><img :src="facebookLogo" alt=""></a>
+        <a href="" class="mr-6"><img :src="twitterLogo" alt=""></a>
+        <a href=""><img :src="pinterestLogo" alt=""></a>
+        </div>
+        <div class="bg-grayblue share-icon" @click="handleShowElement">
+            <img :src="shareIcon" alt="">
+        </div>
+    </div>
+    </Transition>
 </template>
 
-<style type="scss"></style>
+<style>
+.slide-enter-active, .slide-up-leave-active{
+    transition: all 0.25s ease-in;
+}
+
+.slide-enter-from {
+    opacity: 0;
+    transform: translateX(30px);
+}
+
+.slide-leave-to{
+    opacity: 0;
+    transform: translateX(-30px);
+}
+</style>
